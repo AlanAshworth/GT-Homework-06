890946b340c2deaa840ffa502809706296f74277
Alan Ashworth <awashworth927@gmail.com>
2020-03-17 20:16:05 -0400

    Created functional ability to prepend input vales to list of searched cities.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,9 +1,2 @@
 $(document).ready(function() {
-// TODO: Add search icon to search button
-// TODO: remove hardcoded list, add generated list for searches
-
-var apikey = "54a9099a651206645d31affb6bbd4e54";
-var location = "Georgia";
-var queryUrl = toString("https://openweathermap.org/data/2.5/weather?q=" + location + ",us&appid=" + apikey);
-console.log(queryUrl);
 });
